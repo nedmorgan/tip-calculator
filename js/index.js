7aa57a377729d2a870884e63f2f0f14f4a747dfd
nedmorgan <nmorgan1987@gmail.com>
2018-08-15 14:07:22 -0400

    Tip Calculator
    Here is my little tip calculator project

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//calculate tip
+function calculateTip() {
+var receiptTotal = document.getElementById("receipt-input").value;
+
+var tipAmount = document.getElementById("tip-amount").value;
+  
+  //proper input listed
+  if (receiptTotal === "") {
+    alert("Please enter amount");
+    return;
+  }
+  
+  var total = (receiptTotal * (tipAmount / 100));
+  
+  total = Math.round(total * 100) / 100;
+  // have two digits after decimal
+  total = total.toFixed(2);
+  
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+  
+}
+
+document.getElementById("totalTip").style.display = "none";
+
+  document.getElementById("calculate").onclick = function() {
+    calculateTip();
+  };
